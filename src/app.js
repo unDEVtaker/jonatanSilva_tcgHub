acb231a6e291b5e67ea26867e91ed2158271c1a4
unDEVtaker <jonatanemanuelsilva6@gmail.com>
2025-05-16 08:46:11 -0300

    limpiando la consola de los consolelog innecesarios

--- src/app.js
+++ src/app.js
@@ -64,7 +64,7 @@ const findAvailablePort = (startPort, callback) => {
 (async () => {
   try {
     await db.sequelize.authenticate();
-    console.log('Conexión a la base de datos establecida con éxito.');
+    // console.log('Conexión a la base de datos establecida con éxito.');
   } catch (error) {
     console.error('No se pudo conectar a la base de datos:', error);
     process.exit(1); // Salir del proceso si no se puede conectar
@@ -74,7 +74,7 @@ const findAvailablePort = (startPort, callback) => {
 // Conectar a la Base de Datos ANTES de iniciar el servidor
 db.sequelize.authenticate()
   .then(() => {
-    console.log('Conexión a la base de datos establecida correctamente con Sequelize.');
+    // console.log('Conexión a la base de datos establecida correctamente con Sequelize.');
   })
   .then(() => {
     findAvailablePort(process.env.PORT || 3000, (PORT) => {
